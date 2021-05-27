d375059d0fbacf20d0c91a91672bf00e518d64c0
Rubén Onivenis <r.onivenis@preving.com>
2021-05-27 10:49:31 +0200

    cambios para modificar perfil

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { TokenService } from 'src/app/services/token.service';
 import { UsuariosService } from '../../services/usuarios.service';
 
@@ -11,13 +13,19 @@ export class PerfilComponent implements OnInit {
   isLogged = false;
   isAdmin = false;
   nombreUsuario: string;
+  forma_modificar!: FormGroup;
 
   usuario:any = {};
+  usuarioEnviar:any = {};
 
   constructor(
     private _usuariosService: UsuariosService,
-    private tokenService: TokenService
-  ) {}
+    private tokenService: TokenService,
+    private activatedRoute:ActivatedRoute,
+    private formBuilder:FormBuilder
+  ) {
+    this.formulario_modificar();
+  }
 
   ngOnInit(): void {
     this.isLogged = this.tokenService.isLogged();
@@ -38,5 +46,52 @@ export class PerfilComponent implements OnInit {
         err="ERROR";
         console.log(err);
       })
+  }
+
+  formulario_modificar(){
+    this.forma_modificar = this.formBuilder.group({
+      email:['', [Validators.required, Validators.email]],
+      nombre_usuario: ['', [Validators.required, Validators.minLength(5)]],
+      nombre:['', [Validators.required, Validators.minLength(3)]],
+      apellidos:['', [Validators.required, Validators.minLength(5)]],
+      fch_nacimiento:[''],
+      telefono: ['', Validators.pattern("[0-9]{9}")],                                     
+    })
+  }
+
+  modificar(){
+    this.recursivaModificar(this.forma_modificar);
+    if(this.forma_modificar.valid){
+      this.rellenarUsuarios();
+      this.modificarUsuario();
+      location.reload();
+    } 
+  }
+
+  recursivaModificar(item: FormGroup): any{
+    Object.values(item.controls).forEach(control =>{
+      if(control instanceof FormGroup) this.recursivaModificar(control);
+      control.markAsTouched()});
+    return;
+  }
+
+  rellenarUsuarios(){
+    this.usuarioEnviar.email = this.forma_modificar.value.email;
+    this.usuarioEnviar.nombre_usuario = this.forma_modificar.value.nombre_usuario;
+    this.usuarioEnviar.nombre = this.forma_modificar.value.nombre;
+    this.usuarioEnviar.apellidos = this.forma_modificar.value.apellidos;
+    this.usuarioEnviar.fch_nacimiento = this.forma_modificar.value.fch_nacimiento;
+    this.usuarioEnviar.telefono = this.forma_modificar.value.telefono;
+  }
+
+  modificarUsuario(){
+    this._usuariosService.updateUsuario(this.usuarioEnviar, this.activatedRoute.snapshot.params.id)
+      .subscribe(respuesta =>{
+      },
+        (err) => {
+          err="ERROR";
+          console.log(err);
+        } 
+      )
   }
 }
